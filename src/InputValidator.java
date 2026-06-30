9237bb5ced8b7d7cab61a0b1a2a73ea9ebd8ab56
Fea-turing <cabralfea245@gmail.com>
2026-06-30 19:16:17 +0800

    Update InputValidator.java

--- src/InputValidator.java
+++ src/InputValidator.java
@@ -1,7 +1,7 @@
 /* ============================================
  * MODULE 9: Input Validation Module
  * Cinema Ticket Reservation System
- * Author: Group 2 (BSIT 2-3)
+ * Author: Group 1 (BSIT 2-3)
  * Course: COMP 009 Object Oriented Programming
  * Purpose: Validates all user inputs.
             Ensures data integrity before operations.
