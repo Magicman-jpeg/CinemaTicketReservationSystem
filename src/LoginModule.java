94504ee02e4efb0d8fca83bde65790fb93c054bf
Fea-turing <cabralfea245@gmail.com>
2026-06-30 19:16:26 +0800

    Update LoginModule.java

--- src/LoginModule.java
+++ src/LoginModule.java
@@ -1,7 +1,7 @@
 /* ============================================
  * MODULE 1: Login Module
  * Cinema Ticket Reservation System
- * Author: Group 2 (BSIT 2-3)
+ * Author: Group 1 (BSIT 2-3)
  * Course: COMP 009 Object Oriented Programming
  * Purpose: User authentication.
             Includes:
